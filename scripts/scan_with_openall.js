9fa4e119d6f9b2fb1a37db638bdb657559022c0e
chrisjohn404 <chris.s.johnson4@gmail.com>
2016-01-28 08:40:29 -0700

    Made good progress on the device scanner.  Currently properly collecting data from active devices as well as new scanned-for devices.

--- scripts/scan_with_openall.js
+++ scripts/scan_with_openall.js
@@ -1,3 +1,9 @@
+
+
+var async = require('async');
+
+
+var device_curator = require('ljswitchboard-ljm_device_curator');
 var ljm_ffi = require('ljm-ffi');
 var ljm = ljm_ffi.load();
 process.on('uncaughtException', (err) => {
@@ -22,15 +28,68 @@ var foundDevices = {};
 
 // This is an array of curated devices produced by the ljswitchboard-ljm_device_curator module.
 var connectedDevices = [];
+var device;
 
+function connectToLJDevice(callback) {
+	console.log('Connecting to a LJ Device');
+	device = new device_curator.device();
+	device.open('LJM_dtT7','LJM_ctUSB', 'LJM_idANY')
+	.then(function(dev) {
+		console.log('Opened a T7');
+		connectedDevices.push(device);
+		callback();
+	}, function(err) {
+		console.log('Failed to open a T7');
+		callback();
+	});
+}
+
+function closeLJDevice(callback) {
+	device.close()
+	.then(function(dev) {
+		console.log('Closed a T7');
+		callback();
+	}, function(err) {
+		console.log('Failed to close a T7');
+		callback();
+	});
+}
+
+function performOpenAllScan(callback) {
 // Perform Scan
-console.log('Performing Scan with OpenAll');
-deviceScanner.findAllDevices(connectedDevices)
-.then(function(deviceTypes) {
-	ljm.LJM_CloseAll();
-	console.log('Scan Results');
-	console.log(JSON.stringify(deviceTypes, null, 2));
+	console.log('Performing Scan with OpenAll');
+	deviceScanner.findAllDevices(connectedDevices)
+	.then(function(deviceTypes) {
+		console.log('Scan Results');
+		console.log(JSON.stringify(deviceTypes, null, 2));
+		callback();
+	}, function(err) {
+		console.log('Scanning Error', err);
+		callback();
+	});
+}
 
-}, function(err) {
-	console.log('Scanning Error', err);
-});
+function closeAllDevices(callback) {
+	ljm.LJM_CloseAll();
+	callback();
+}
+
+var operations = [
+	connectToLJDevice,
+	performOpenAllScan,
+	closeLJDevice,
+	closeAllDevices,
+];
+
+async.eachSeries(operations,
+	function executor(func, cb) {
+		if(typeof(func) === 'function') {
+			func(cb);
+		} else {
+			console.log('Operation is not a function...');
+			cb();
+		}
+	},
+	function finishedExecuting() {
+		console.log('Finished Executing...');
+	});
